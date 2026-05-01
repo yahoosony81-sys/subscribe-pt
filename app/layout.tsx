e5191171fbdb1183661b9078229c2e1096da788b
Minjae <yahoosony81@gmail.com>
2026-05-01 18:08:45 +0900

    픽셀과 gtm코드 모두 노형점으로수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const notoSansKr = Noto_Sans_KR({
 });
 
 export const metadata: Metadata = {
-  title: '마인드 휘트니스 한림점 | 5월 가정의 달 프로모션',
-  description: '어머니 아버님께 근육을 선물하세요! 마인드 휘트니스 한림점 5월 가정의 달 특별 프로모션',
+  title: '마인드 휘트니스 노형점 | 5월 가정의 달 프로모션',
+  description: '어머니 아버님께 근육을 선물하세요! 마인드 휘트니스 노형점 5월 가정의 달 특별 프로모션',
   generator: 'v0.app',
 }
 
@@ -24,29 +24,51 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        {/* --- [1] GTM 헤드 코드 삽입 시작 --- */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
+        {/* ── 노형점 전용 GTM (GTM-TGKR77FL) ── */}
+        <Script id="gtm" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5VB56Q69');
-          `}
-        </Script>
-        {/* --- GTM 헤드 코드 삽입 끝 --- */}
+            })(window,document,'script','dataLayer','GTM-TGKR77FL');
+          `
+        }} />
+
+        {/* ── 노형점 전용 Meta Pixel (891686733924318) ── */}
+        <Script id="meta-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '891686733924318');
+            fbq('track', 'PageView');
+          `
+        }} />
       </head>
       <body className={`${notoSansKr.variable} font-sans antialiased`}>
-        {/* --- [2] GTM 바디 코드(noscript) 삽입 시작 --- */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5VB56Q69"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TGKR77FL"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        {/* --- GTM 바디 코드 삽입 끝 --- */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=891686733924318&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
 
         {children}
         <Analytics />
