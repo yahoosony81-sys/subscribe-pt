741cacd6a01da1eac28ddefd98bae602a7858768
Minjae <yahoosony81@gmail.com>
2026-05-07 15:19:34 +0900

    feat: connect bodychallenge form to new google sheets url

--- app/promo-bodychallenge/page.tsx
+++ app/promo-bodychallenge/page.tsx
@@ -1,6 +1,6 @@
 import { PromotionBodychallenge } from "@/components/promotions/promotion-bodychallenge"
 import { LocationSectionBodychallenge } from "@/components/location-section-bodychallenge"
-import { RegistrationSection } from "@/components/registration-section"
+import { RegistrationSectionBodychallenge } from "@/components/registration-section-bodychallenge"
 import { FooterBodychallenge } from "@/components/footer-bodychallenge"
 
 export default function PromoBodychallengePage() {
@@ -8,7 +8,7 @@ export default function PromoBodychallengePage() {
     <main className="min-h-screen">
       <PromotionBodychallenge />
       <LocationSectionBodychallenge />
-      <RegistrationSection title="온라인 참가 신청하기" branch="도남점" hideTimePicker={true} />
+      <RegistrationSectionBodychallenge title="온라인 참가 신청하기" branch="도남점" hideTimePicker={true} />
       <FooterBodychallenge />
     </main>
   )
