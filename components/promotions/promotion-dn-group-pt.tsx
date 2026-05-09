ecd790aaaf746979d997dc96546f5500c832fd30
Minjae <yahoosony81@gmail.com>
2026-05-09 14:49:18 +0900

    GTM 테그메니저설치

--- components/promotions/promotion-dn-group-pt.tsx
+++ components/promotions/promotion-dn-group-pt.tsx
@@ -46,6 +46,19 @@ export function PromotionDnGroupPt() {
 
   return (
     <div id="promo-dn-group-pt" className="promotion-dn-container bg-white font-sans text-slate-900 tracking-tight">
+      <Script id="gtm-dn-group-pt" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KQR8JJ44');
+        `
+      }} />
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQR8JJ44"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+      </noscript>
       {pixelId && (
         <>
           <Script id="meta-pixel-dn-group-pt" strategy="afterInteractive" dangerouslySetInnerHTML={{
